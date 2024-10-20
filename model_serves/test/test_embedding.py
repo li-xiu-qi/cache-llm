8099362a363903b5f59ef30df68fecf212b67c4a
Li Xiuqi <li.xiu.qi@example.com>
2024-10-20 17:12:45 +0800

    init

---
+++ model_serves/test/test_embedding.py
@@ -0,0 +1,31 @@
+from model_serves.config.app_config import ModelConfig
+from model_serves.config import rag_config
+from model_serves.embedding_model import EmbeddingModel
+from model_serves.types import EmbeddingInput
+config = ModelConfig(
+    embedding_model_name="BAAI/bge-m3",
+    llm_api_configs=rag_config.guiji_api_configs,
+    )
+from model_serves.chat_model import ChatModel
+from model_serves.client_manager import ClientManager
+
+
+
+
+if __name__=="__main__": 
+    import asyncio
+
+    client_manager = ClientManager(config.llm_api_configs)
+  
+    from diskcache import Cache
+    
+    cache = Cache("./cache")
+    embedding_model = EmbeddingModel(client_manager, cache)
+    
+    async def main():
+        res = await embedding_model.embedding(model_input=EmbeddingInput(name=config.embedding_model_name, 
+                                                                    input_content=[ "你好",  ]
+                      ))
+        print(res)
+    asyncio.run(main())
+        
---
+++ model_serves/test/test_llm.py
@@ -0,0 +1,33 @@
+from model_serves.config.app_config import ModelConfig
+from model_serves.config import rag_config
+from model_serves.types import LLMInput, Message
+config = ModelConfig(
+    model_name="glm-4-flash",
+    llm_api_configs=rag_config.zhipu_api_configs,
+    )
+from model_serves.chat_model import ChatModel
+from model_serves.client_manager import ClientManager
+
+
+
+
+if __name__=="__main__": 
+    import asyncio
+
+    client_manager = ClientManager(config.llm_api_configs)
+  
+    from diskcache import Cache
+    
+    cache = Cache("./cache")
+    
+    chat_model = ChatModel(client_manager, cache)
+    async def main():
+        res = await chat_model.chat(model_input=LLMInput(name=config.model_name, 
+                                                                    input_content=[
+                                                                        Message(role="user", content="what is your name!"), 
+                                                                        ]
+                                                      )
+                      )
+        print(res)
+    asyncio.run(main())
+        
